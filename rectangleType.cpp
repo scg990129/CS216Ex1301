457fd99c0f1cf68838c72c48546590a98fdcb5e4
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-11 17:06:43 -0700

    Initial commit

---
+++ rectangleType.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jacob Yeung on 10/11/23.
+//
+
+#include "rectangleType.h"
---
+++ rectangleType.h
@@ -0,0 +1,16 @@
+//
+// Created by Jacob Yeung on 10/11/23.
+//
+
+#ifndef UNTITLED_RECTANGLETYPE_H
+#define UNTITLED_RECTANGLETYPE_H
+
+
+
+class rectangleType {
+
+};
+
+
+
+#endif //UNTITLED_RECTANGLETYPE_H
