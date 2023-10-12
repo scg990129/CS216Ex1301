cbb70aec528f7e4eb0a92756bf8c110b9f7fb99c
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-11 18:09:27 -0700

    == != completed -- ++ completed

--- rectangleType.cpp
+++ rectangleType.cpp
@@ -3,10 +3,11 @@
 //
 
 #include "rectangleType.h"
+#include <iostream>
 
-rectangleType::rectangleType(double h, double w) {
-    height = h;
-    width = w;
+rectangleType::rectangleType(int h, int w) {
+    this->height = h;
+    this->width = w;
 }
 
 bool rectangleType::operator==(const rectangleType& other) const{
@@ -19,3 +20,56 @@ bool rectangleType::operator!=(const rectangleType& other) const{
            other.height * other.width;
 }
 
+bool rectangleType::operator>=(const rectangleType& other) const{
+    return this->height * this->width >=
+           other.height * other.width;
+}
+bool rectangleType::operator<=(const rectangleType& other) const{
+    return this->height * this->width <=
+           other.height * other.width;
+}
+bool rectangleType::operator>(const rectangleType& other) const{
+    return this->height * this->width >
+           other.height * other.width;
+}
+bool rectangleType::operator<(const rectangleType& other) const{
+    return this->height * this->width <
+           other.height * other.width;
+}
+
+rectangleType& rectangleType::operator++(){ //is correct. It is a prefix increment operator,
+    ++this->height;
+    ++this->width;
+    return *this;
+}
+
+const rectangleType rectangleType::operator++(int number) {
+    rectangleType temp(this->height, this->width);
+    ++this->height;
+    ++this->width;
+    return temp;
+}
+
+rectangleType& rectangleType::operator--(){ //is correct. It is a prefix increment operator,
+    if(this->height == 0 || this->width == 0){
+        std::cout << "Error: height or width is 0" << std::endl;
+    }
+
+    this->height -= this->height == 0 ? 0 : 1;
+    this->width -= this->width == 0 ? 0 : 1;
+    return *this;
+}
+
+const rectangleType rectangleType::operator--(int number) {
+    rectangleType temp(this->height, this->width);
+
+
+    if(this->height == 0 || this->width == 0){
+        std::cout << "Error: height or width is 0" << std::endl;
+    }
+
+    this->height -= this->height == 0 ? 0 : 1;
+    this->width -= this->width == 0 ? 0 : 1;
+
+    return temp;
+}
--- rectangleType.h
+++ rectangleType.h
@@ -7,11 +7,21 @@
 
 class rectangleType {
 protected:
-    double height, width;
+    int height, width;
 public:
-    rectangleType(double h = 0, double w = 0);
+    rectangleType(int h = 0, int w = 0);
     bool operator==(const rectangleType& other) const;
     bool operator!=(const rectangleType& other) const;
+    bool operator>=(const rectangleType& other) const;
+    bool operator<=(const rectangleType& other) const;
+    bool operator>(const rectangleType& other) const;
+    bool operator<(const rectangleType& other) const;
+
+    rectangleType& operator++(); //is correct. It is a prefix increment operator,
+    const rectangleType operator++(int number);
+    rectangleType& operator--(); //is correct. It is a prefix increment operator,
+    const rectangleType operator--(int number);
+
 
 //    rectangleType& operator++() const;
 //    rectangleType operator++(const int number) const;
